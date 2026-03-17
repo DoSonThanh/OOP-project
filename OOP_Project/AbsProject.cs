e7314e5373e6b7ee3c2967524eb31c0a968d15d3
Netherscreech <nguyentuankhoiteleman@gmail.com>
2026-03-17 11:24:49 +0700

    Updated Encapsulation

--- OOP_Project/AbsProject.cs
+++ OOP_Project/AbsProject.cs
@@ -2,13 +2,14 @@ using System;
 
 public abstract class AbsProject
 {
-    public string projectId { get; set; }
-    public string projectName { get; set; }
-    public DateTime startDate { get; set; }
-    public DateTime endDate { get; set; }
-    public string description { get; set; }
-    public EnumStatus status { get; set; }
-    protected AbsProject(string projectId, string projectName)
+
+      public string projectId { get; set; }
+      public string projectName { get; set; }
+      public DateTime startDate { get; set; }
+      public DateTime endDate { get; set; }
+      public string description { get; set; }
+      public EnumStatus status { get; set; }
+      protected AbsProject(string projectId, string projectName)
 
     {
         this.projectId = projectId;
