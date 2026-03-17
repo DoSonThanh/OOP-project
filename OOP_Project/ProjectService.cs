ac82083a56fa28d9fa732e5984fc75422719ccd8
kannar04 <manhle240406@gmail.com>
2026-03-17 14:28:09 +0700

    fix
    Removed old implementation of ProjectService and retained the interface.

--- OOP_Project/ProjectService.cs
+++ OOP_Project/ProjectService.cs
@@ -1,37 +1,4 @@
 using System;
-
-public class ProjectService : IProjectService // (Giả sử bạn đã sửa tên interface cho đúng chính tả)
-{
-    // Thể hiện Aggregation: Service "chứa" (giữ 1 tham chiếu tới) DataStorage
-    private DataStorage _storage;
-
-    // Constructor nhận DataStorage từ bên ngoài truyền vào (Dependency Injection)
-    public ProjectService(DataStorage storage)
-    {
-        this._storage = storage;
-    }
-
-    public void CreateProject(ProjectBuilder builder)
-    {
-
-        Console.WriteLine("Đã thêm project mới vào DataStorage.");
-    }
-
-    public void UpdateProject()
-    {
-        // Logic update
-    }
-
-    public void DeleteProject()
-    {
-        // Logic delete
-    }
-
-    public void GetProject()
-    {
-        // Logic get
-    }
-}
 using System.Collections.Generic;
 
 public class ProjectService : IProjectService
