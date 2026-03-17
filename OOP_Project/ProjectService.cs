a3463c59f7eea9ca787c569139ef2acc631a4570
kannar04 <manhle240406@gmail.com>
2026-03-17 14:08:37 +0700

    aggregation

--- OOP_Project/ProjectService.cs
+++ OOP_Project/ProjectService.cs
@@ -1,11 +1,34 @@
-public class ProjectService : IProjectService
-{
+using System;
 
-    ProjectService()
+public class ProjectService : IProjectService // (Giả sử bạn đã sửa tên interface cho đúng chính tả)
+{
+    // Thể hiện Aggregation: Service "chứa" (giữ 1 tham chiếu tới) DataStorage
+    private DataStorage _storage;
+
+    // Constructor nhận DataStorage từ bên ngoài truyền vào (Dependency Injection)
+    public ProjectService(DataStorage storage)
     {
+        this._storage = storage;
     }
-    public void CreateProject(ProjectBuilder builder){}
-    public void UpdateProject(){}
-    public void DeleteProject(){}
-    public void GetProject(){}
+
+    public void CreateProject(ProjectBuilder builder)
+    {
+
+        Console.WriteLine("Đã thêm project mới vào DataStorage.");
+    }
+
+    public void UpdateProject()
+    {
+        // Logic update
+    }
+
+    public void DeleteProject()
+    {
+        // Logic delete
+    }
+
+    public void GetProject()
+    {
+        // Logic get
+    }
 }
