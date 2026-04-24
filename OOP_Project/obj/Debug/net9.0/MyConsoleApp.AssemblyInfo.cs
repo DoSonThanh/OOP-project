0153751c4579e828b8c722a30006a2914be1a3b2
Kannar04 <manhle240406@gmail.com>
2026-04-24 15:46:48 +0700

    code refactored

--- OOP_Project/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ OOP_Project/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+657b24ea313525ff86ee2d51e6ab2cf2225d839d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28de9a3f9418e219e8f22e66834e42c4c143b1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
